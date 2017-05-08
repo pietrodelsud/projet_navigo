4f647871315b4f7d5b892d8e09a03bed3fadd388
pietrodelsud <pietrodelsud@hotmail.com>
2017-05-08 21:17:38 +0200

    Add files via upload

---
+++ global.R
@@ -0,0 +1,4 @@
+library(data.table)
+
+don <- fread("/Users/ingacarrelet/formation_CEPE/projet_groupe/Coord_gares_transilien 2015 10  12.csv",header=TRUE,sep=";")
+
---
+++ ui.R
@@ -0,0 +1,11 @@
+library(shiny)
+library(leaflet)
+
+
+
+fluidPage(   
+           leafletOutput("mymap")
+                        
+          )
+                                      
+                                      
